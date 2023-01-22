cf127708234ac954208de782fcedc6430919423a
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-01-22 22:04:39 +0100

    bad update

--- test one/src/main.js
+++ test one/src/main.js
@@ -1,10 +1,13 @@
 const buttonL= document.getElementById("about");
 const outputL = document.getElementById("left");
 buttonL.addEventListener('click', function (){
-outputL.classList.toggle('hadden');
+outputL.classList.toggle('hadden')
+outputL.scrollIntoView();
 });
+
 const buttonR= document.getElementById("contact");
 const outputR = document.getElementById("right");
 buttonR.addEventListener('click', function (){
-outputR.classList.toggle('hadden');
+outputR.classList.toggle('hadden')
+outputR.scrollIntoView("");
 });
