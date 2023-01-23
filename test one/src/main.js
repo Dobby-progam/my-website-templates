1b277c987897e10f68cb08768590b94ea24fbed9
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-01-23 18:26:46 +0100

    added some JS and clean up

--- test one/src/main.js
+++ test one/src/main.js
@@ -1,13 +1,28 @@
+//display the about and scroll to it
 const buttonL= document.getElementById("about");
-const outputL = document.getElementById("left");
+const left = document.getElementById("left");
 buttonL.addEventListener('click', function (){
-outputL.classList.toggle('hadden')
-outputL.scrollIntoView();
+left.classList.toggle('hadden')
+left.scrollIntoView();
 });
-
+//display the contact page and scroll to it
 const buttonR= document.getElementById("contact");
 const outputR = document.getElementById("right");
 buttonR.addEventListener('click', function (){
 outputR.classList.toggle('hadden')
 outputR.scrollIntoView("");
+});
+//scroll up and hide the about 
+const butabout= document.getElementById("butabout");
+const up=document.getElementById("up");
+butabout.addEventListener('click', function(){
+up.scrollIntoView("");
+left.classList.toggle("hadden");
+});
+//scroll up and hide the contact page
+const butcont=document.getElementById("butcont")
+butcont.addEventListener('click', function(){
+    up.scrollIntoView
+    right.classList.toggle('hadden')
+
 });
